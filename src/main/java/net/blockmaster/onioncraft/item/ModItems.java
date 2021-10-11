df05e20e6e91ecf6e171a8a55ef23c50fc76e536
Blockmaster2118 <49419708+Blockmaster2118@users.noreply.github.com>
2021-10-11 13:49:36 +1100

    added Inferno

--- src/main/java/net/blockmaster/onioncraft/item/ModItems.java
+++ src/main/java/net/blockmaster/onioncraft/item/ModItems.java
@@ -15,11 +15,12 @@ public class ModItems {
     public static final Item FIRE_DRACONIA_CRYSTAL = registerItem("fire_draconia_crystal",
             new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
-    public static final ToolItem INFERNO = new SwordItem(DraconiaMaterial.INSTANCE, 7, 2.6f, new FabricItemSettings().group(ItemGroup.COMBAT));
+    public static final ToolItem INFERNO = (ToolItem) registerItem("inferno", new SwordItem(DraconiaMaterial.INSTANCE, 7, 2.6f, new FabricItemSettings().group(ItemGroup.COMBAT)));
 
     private static Item registerItem(String name,Item item) {
         return Registry.register(Registry.ITEM, new Identifier(OnionCraft.Mod_ID, name), item);
     }
+
 
     public static void registerModItems() {
         System.out.println("Registering Mod Items for "+ OnionCraft.Mod_ID);
